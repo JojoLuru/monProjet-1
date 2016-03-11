d715627737cec8d65fe6f827bb589a56e8392138
Delbiaggio Nicolas <delbiaggionicolas@MacBook-Pro-de-Delbiaggio.local>
2016-03-11 10:33:48 +0100

    2 commit avec -am

--- src/classes/Departement.java
+++ src/classes/Departement.java
@@ -1,15 +1,7 @@
 package classes;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//Yes we can
 
-/**
- *
- * @author delbiaggionicolas
- */
 public class Departement {
     private int noDept;
     private String nomDept;
